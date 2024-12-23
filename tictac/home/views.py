af0bd784bcf1f68783ad597fc4b33a3194cba189
haseeb4339 <haseebullahbj@gmail.com>
2024-12-23 12:18:39 +0500

    game create functionality

--- tictac/home/views.py
+++ tictac/home/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import *
 
 def home(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        option = request.POST.get('option')
+        room_code = request.POST.get('room_code')
+
+        if option == '1':
+            game = Game.objects.filter(room_code=room_code).first()
+            if game is None:
+                message.success(request, 'Room code is not found')
+                return redirect('home')
+            if game.is_over:
+                message.success(request, 'Game is over')
+                return redirect('home')
+            game.game_opponent = username
+            game.save()
+        else:
+            game = Game(game_creator = username, room_code = room_code)
+            game.save()
+            return redirect('/play/' + room_code + '?username='+username)
     return render(request, 'home.html')
 
 
