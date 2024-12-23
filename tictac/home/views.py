c6796d01fd8c0e93d30212ecf5a2f239c859f64f
haseeb4339 <haseebullahbj@gmail.com>
2024-12-23 11:56:32 +0500

    create a functions in views.py file

--- tictac/home/views.py
+++ tictac/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def play(request, room_code):
+    return render(request, 'play.html', context)
